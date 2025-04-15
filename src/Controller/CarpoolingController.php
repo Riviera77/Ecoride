2265462cf09087138d01b4fc1c3b4746c7cf64c5
Grey <ingrid.freguis@gmail.com>
2025-04-15 19:23:58 +0200

    update path

--- src/Controller/CarpoolingController.php
+++ src/Controller/CarpoolingController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\CarpoolingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/carpooling', name: 'carpooling_')]
 final class CarpoolingController extends AbstractController
 {
-    #[Route('/carpooling', name: 'app_carpooling')]
+    #[Route('/search', name: 'index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('carpooling/index.html.twig', [
-            'controller_name' => 'CarpoolingController',
             'title' => 'Carpooling',
+        ]);
+    }
+
+    #[Route('/list', name: 'list', methods: ['GET'])]
+    public function list(CarpoolingRepository $carpooling): Response
+    {
+        dd($carpooling->findAll());
+        return $this->render('carpooling/list.html.twig', [
+            'title' => 'Carpooling',
+            'carpoolings' => $carpooling->findAll(),
         ]);
     }
 }
