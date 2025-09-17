be59454634cea2cf9b8f41568f62b561f5a6ddfd
Grey <ingrid.freguis@gmail.com>
2025-09-17 17:00:48 +0200

    initial bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,15 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Ne charger .env que si APP_ENV n’est pas déjà défini (cas Heroku = prod)
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
-    if (method_exists(Dotenv::class, 'bootEnv')) {
-        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-    } else {
-        (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
-    }
-}
-
-if ($_SERVER['APP_DEBUG'] ?? false) {
-    umask(0000);
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
