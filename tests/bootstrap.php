e78256830f599b2fa1633399acefd8a83efd2daf
Grey <ingrid.freguis@gmail.com>
2025-09-19 09:37:04 +0200

    modify bootstrap.php : to solve KERNEL_CLASS environment variable problem

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Ne charger .env que si APP_ENV non défini (Heroku le définit)
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
-    if (method_exists(Dotenv::class, 'bootEnv')) {
-        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-    } else {
-        (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
-    }
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
